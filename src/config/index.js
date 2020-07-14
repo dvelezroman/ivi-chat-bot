1a50514acfad6b32e96f55b8291931661cebdc3f
Dario Velez <dvelezroman@gmail.com>
2020-07-14 09:12:32 -0300

    style initial message

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,28 @@
-// Config starter code
+import React from 'react';
 import { createChatBotMessage } from 'react-chatbot-kit';
+import { LearningOptions } from '../Components/LearningOptions';
 
 const config = {
-  initialMessages: [createChatBotMessage(`Hi`)],
+  botName: 'IVI Learning Bot',
+  initialMessages: [
+    createChatBotMessage(`Hi, I'm Ivi. I am here to help. What do you want to learn?`, {
+      widget: 'learningOptions',
+    }),
+  ],
+  customStyles: {
+    botMessageBox: {
+      backgroundColor: '#376B7E',
+    },
+    chatButton: {
+      backgroundColor: '#376B7E',
+    },
+  },
+  widgets: [
+    {
+      widgetName: 'learningOptions',
+      widgetFunc: (props) => <LearningOptions {...props} />,
+    },
+  ],
 };
 
 export default config;
