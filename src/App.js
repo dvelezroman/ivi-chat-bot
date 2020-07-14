a36d7125f06790440e2ac4f9cf6dad5fad45740b
Dario Velez <dvelezroman@gmail.com>
2020-07-14 00:22:57 -0300

    bot set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import Chatbot from 'react-chatbot-kit';
 import './App.css';
+import config from './config';
+import ActionProvider from './Components/ActionProvider';
+import MessageParser from './Components/MessageParser';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Chatbot
+          config={config}
+          actionProvider={ActionProvider}
+          messageParser={MessageParser}
+        />
       </header>
     </div>
   );
---
+++ src/Components/ActionProvider.js
@@ -0,0 +1,21 @@
+// ActionProvider starter code
+class ActionProvider {
+  constructor(createChatBotMessage, setStateFunc) {
+    this.createChatBotMessage = createChatBotMessage;
+    this.setState = setStateFunc;
+  }
+
+  greet() {
+    const greetingMessage = this.createChatBotMessage('Hi, how are you?');
+    this.updateChatBotState(greetingMessage);
+  }
+
+  updateChatBotState(message) {
+    this.setState((prevState) => ({
+      ...prevState,
+      messages: [...prevState.messages, message],
+    }));
+  }
+}
+
+export default ActionProvider;
